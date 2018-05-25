ee89d8a230b04ca630e275dc0cf5b4a71b837e51
P0nj4 <german.f.pereyra@gmail.com>
2018-05-25 12:31:39 -0300

    move featured to its own file

---
+++ lib/home_components/featured.dart
@@ -0,0 +1,61 @@
+import 'package:cbd_food/utils/shadow_text.dart';
+import "package:flutter/material.dart";
+import "package:carousel/carousel.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class Featured extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        child: new Stack(
+          children: <Widget>[
+            new Container(
+                margin: EdgeInsets.only(bottom: 10.0),
+                child: asyncCarousel(),
+              ),
+              new Positioned(
+                  bottom: 0.0,
+                  right: -10.0,
+                  child: new Image(
+                    image: new AssetImage('assets/images/featured.png'),
+                    width: 106.0,
+                    height: 35.0,
+                  )
+              ),
+          ],
+        )
+    );
+  }
+
+  Widget _carouselContent(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return new SizedBox(
+      height: 220.0,
+      child: new Carousel(
+        children: snapshot.data.documents.map((DocumentSnapshot doc) {
+          return new Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              new Image.network(doc['image_url'],
+                  height: 220.0, fit: BoxFit.cover),
+              new ShadowText(doc['name'],
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 32.0,
+                  ))
+            ],
+          );
+        }).toList(),
+        displayDuration: const Duration(seconds: 3),
+      ),
+    );
+  }
+
+  Widget asyncCarousel() {
+    return new StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('featured').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return new Text('Loading...');
+          return _carouselContent(snapshot);
+        });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
+import 'package:cbd_food/home_components/featured.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:carousel/carousel.dart';
-import 'dart:ui' as ui;
 
-void main() => runApp(new MyApp2());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -38,101 +36,11 @@ class MyHomePage extends StatelessWidget {
       ),
     );
 
-    Widget _testCarousel(AsyncSnapshot<QuerySnapshot> snapshot) {
-      return new SizedBox(
-        height: 220.0,
-        child: new Carousel(
-          children: snapshot.data.documents
-              .map((DocumentSnapshot doc) {
-                return new Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[
-                    new Image.network(doc['image_url'], height: 220.0, fit: BoxFit.cover),
-                    new ShadowText(doc['name'],
-                      style: new TextStyle(
-                        color: Colors.white,
-                        fontSize: 32.0,
-                      )
-                    )
-                  ],
-                );
-              })
-              .toList(),
-          displayDuration: const Duration(seconds: 3),
-        ),
-      );
-    }
-
-    Widget asyncCarousel() {
-      return new StreamBuilder<QuerySnapshot>(
-          stream: Firestore.instance.collection('featured').snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) return new Text('Loading...');
-            return _testCarousel(snapshot);
-          });
-    }
-
-    Widget test = new Container(
-        decoration: new BoxDecoration(
-          color: Colors.white,
-        ),
-        child: new Column(
-          children: <Widget>[
-            new Stack(
-              children: <Widget>[
-                new Container(
-                  margin: EdgeInsets.only(bottom: 10.0),
-                  child: asyncCarousel(),
-                ),
-                new Positioned(
-                  bottom: 0.0,
-                  right: -10.0,
-                  child: new Image(
-                    image: new AssetImage('assets/images/featured.png'),
-                    width: 106.0,
-                    height: 35.0,
-                  )
-                ),
-              ],
-            ),
-            new Text('41'),
-          ],
-        ));
-
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("CBD Food"),
         ),
-        body: test);
-  }
-}
-
-class ShadowText extends StatelessWidget {
-  ShadowText(this.data, { this.style }) : assert(data != null);
-
-  final String data;
-  final TextStyle style;
-
-  Widget build(BuildContext context) {
-    return new ClipRect(
-      child: new Stack(
-        children: [
-          new Positioned(
-            top: 2.0,
-            left: 2.0,
-            child: new Text(
-              data,
-              style: style.copyWith(color: Colors.black.withOpacity(0.5)),
-            ),
-          ),
-          new BackdropFilter(
-            filter: new ui.ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
-            child: new Text(data, style: style),
-          ),
-        ],
-      ),
-    );
+        body: new Featured());
   }
 }
 
