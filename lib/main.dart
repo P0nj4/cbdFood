91a186602b0fbee8ceb213cd6ced7a8030fd8068
P0nj4 <german.f.pereyra@gmail.com>
2018-06-02 09:37:18 -0300

    adding scrollcontroller

--- lib/main.dart
+++ lib/main.dart
@@ -31,26 +31,84 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
+
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = new ScrollController();
+    _scrollController.addListener((){
+      print(_scrollController.offset);
+      if (_scrollController.position.maxScrollExtent - _scrollController.offset < 10.0) {
+
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new CustomScrollView(
+//        physics: const AlwaysScrollableScrollPhysics(), // new
         slivers: <Widget>[
           new SliverAppBar(
-            title: Text('nav bar title here'),
-            floating: true,
+            title: Text(widget.recipe.name),
+            floating: false,
             pinned: true,
             expandedHeight: 250.0,
             flexibleSpace: new FlexibleSpaceBar(
-              title: Text('nav bar title here 2'),
               background:
-              new Container(
-                color: Colors.red,
+              new Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  new ClipPath(
+                    clipper: new ArcClipper(),
+                    child: new Image.network(widget.recipe.imageUrl, fit: BoxFit.cover,),
+                  ),
+                  const DecoratedBox(
+                    decoration: const BoxDecoration(
+                      gradient: const LinearGradient(
+                        begin: const Alignment(0.0, -0.5),
+                        end: const Alignment(0.0, 0.9),
+                        colors: const <Color>[Color(0x80ffffff), Color(0x10ffffff), Color(0x00ffffff)],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
         ],
+//        controller: _scrollController,
       ),
     );
   }
+}
+
+class ArcClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+    path.lineTo(0.0, size.height - 30);
+
+    var firstControlPoint = new Offset(size.width / 4, size.height);
+    var firstPoint = new Offset(size.width / 2, size.height);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstPoint.dx, firstPoint.dy);
+
+    var secondControlPoint =
+    new Offset(size.width - (size.width / 4), size.height);
+    var secondPoint = new Offset(size.width, size.height - 30);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondPoint.dx, secondPoint.dy);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
